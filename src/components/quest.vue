7b7ae13846d1965c140fadc2f7ab73ab6172d5e1
Leonid Nevezhin <LNevezhin@gmail.com>
2020-06-05 15:08:13 +0300

    countries v2.0

--- src/components/quest.vue
+++ src/components/quest.vue
@@ -1,64 +1,60 @@
 <template>
   <div class="container" :key="activeCard">
     <div>
-      <div class="row">
-        <div class="col-6 offset-3"><img :src="imageUrl" /></div>
-      </div>
-      <div class="row">
-        <div class="col-6 offset-4 mt-4"></div>
+      <div id="empty">пустота</div>
+      <div v-for="(quest, index) in activeQuestCard" :key="parseInt(index)">
+        <div class="row align-items-center">
+          <div class="col-1 offset-4">
+            <div class="badge badge-pill badge-danger">{{ index + 1 }}</div>
+          </div>
+          <div id="country" class="col">{{randomCountry[index]}}</div>
+        </div>
       </div>
       <div>
-        <div v-for="(quest, index) in activeQuestCard" :key="parseInt(index)">
-          <div class="row mt-4">
-            <div class="col-1 offset-4">
-              <div class="badge badge-pill badge-danger">{{ index + 1 }}</div>
-            </div>
-            <div class="col-3">
-              <input
-                :ref="parseInt(index)"
-                :id="parseInt(index)"
-                type="text"
-                class="form-control"
-                v-bind:class="inputStatus[index]"
-                v-model.trim="inputData[index]"
-                v-bind:disabled="inputStatus[index] == 'is-valid'"
-                @input="checkInput(index)"
-                @focus="getFocus(index)"
-                @blur="lostFocus(index)"
-                autocomplete="off"
-              />
-              <div class="valid-feedback">
-                Верно!
-              </div>
-              <div class="invalid-feedback">
-                Введите правильный ответ!
-              </div>
-            </div>
-            <div class="col">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                v-bind:class="{ 'sr-only': hintStatus[index] == false }"
-                @click="giveLetter(index)"
-              >
-                ?
-              </button>
-            </div>
+        <div id="empty">пустота</div>
+        <div class="row mt-4" v-for="(quest, index) in activeQuestCard" :key="parseInt(index)">
+          <div class="col-1 offset-4">
+            <div class="badge badge-pill badge-danger">{{ index + 1 }}</div>
+          </div>
+          <div class="col-3">
+            <input
+              :ref="parseInt(index)"
+              :id="parseInt(index)"
+              type="text"
+              class="form-control"
+              v-bind:class="inputStatus[index]"
+              v-model.trim="inputData[index]"
+              v-bind:disabled="inputStatus[index] == 'is-valid'"
+              @input="checkInput(index)"
+              @focus="getFocus(index)"
+              @blur="lostFocus(index)"
+              autocomplete="off"
+            />
+            <div class="valid-feedback">Верно!</div>
+            <div class="invalid-feedback">Введите правильный ответ!</div>
+          </div>
+          <div class="col">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              v-bind:class="{ 'sr-only': hintStatus[index] == false }"
+              @click="giveLetter(index)"
+            >?</button>
           </div>
         </div>
-        <div class="col-1 mb-4 offset-5 mt-3">
-          <button
-            id="next"
-            type="button"
-            class="btn btn-secondary"
-            v-bind:class="{ 'sr-only': nextStatusButton == true }"
-            @click="getNextCard()"
-          >
-            Далее
-          </button>
-        </div>
+      </div>
+      <div class="col-1 mb-4 offset-5 mt-3">
+        <button
+          id="next"
+          type="button"
+          class="btn btn-secondary"
+          v-bind:class="{ 'sr-only': nextStatusButton == true }"
+          @click="getNextCard()"
+        >Далее</button>
       </div>
     </div>
+    <div id="empty">пустота</div>
+    <!--     <div id="count">{{this.letterCount}}</div> -->
   </div>
 </template>
 
@@ -67,7 +63,7 @@ export default {
   name: "quest",
   data() {
     return {
-      activeUrl: "",
+      randomCountry: [],
       activeQuestCard: [],
       inputData: [],
       correctInputLetters: [], // массив с правильно введенными буквами
@@ -80,52 +76,243 @@ export default {
       nextStatusButton: true,
       timerStopInput: [],
       timerFocusLost: [],
-      activeCard: 0, // номер текущей карточки, оно же ключ, для обновления ДОМ
+      activeCard: 0,
+      totalLetters: 0,
+      letterCount: 0,
+      randomCountryIndex: [], // номер текущей карточки, оно же ключ, для обновления ДОМ
       ajaxApi: [
-        [
-          "0",
-          "pic0000.png",
-          "ПоМиДоР",
-          "КаПуСтА",
-          "пЕрЕц",
-          "БаКЛАЖан",
-          "ЧЕСНОК",
-        ],
-        [
-          "1",
-          "pic0001.png",
-          "Москва",
-          "Киев",
-          "Минск",
-          "Баку",
-          "Ереван",
-          "Тбилиси",
-          "Алматы",
-        ],
-        ["2", "pic0003.png", "красныЙ", "белыЙ", "черныЙ"],
-        [
-          "3",
-          "pic0002.png",
-          "КруГ",
-          "КвадраТ",
-          "ТреугольниК",
-          "РомБ",
-          "ТрапециЯ",
-          "ЭллипС",
-        ],
-      ],
+        ["Андорра", "Андорра-ла-Велья"],
+        ["ОАЭ", "Абу-Даби"],
+        ["Афганистан", "Кабул"],
+        ["Антигуа и Барбуда", "Сент-Джонс"],
+        ["Ангилья", "Валли"],
+        ["Албания", "Тирана"],
+        ["Армения", "Ереван"],
+        ["Ангола", "Луанда"],
+        ["Аргентина", "Буэнос-Айрес"],
+        ["Американское Самоа", "Пагопаго"],
+        ["Австрия", "Вена"],
+        ["Австралия", "Канберра"],
+        ["Аруба", "Ораньестад"],
+        ["Азербайджан", "Баку"],
+        ["Босния", "Сараево"],
+        ["Барбадос", "Бриджтаун"],
+        ["Бангладеш", "Дакка"],
+        ["Бельгия", "Брюссель"],
+        ["Буркина Фасо", "Уагадугу"],
+        ["Болгария", "София"],
+        ["Бахрейн", "Манама"],
+        ["Бурунди", "Бужумбура"],
+        ["Бенин", "Порто-Ново"],
+        ["Бермудские о-ва", "Гамильтон"],
+        ["Бруней", "Бандар-Сери-Бегаван"],
+        ["Боливия", "Сукре"],
+        ["Бразилия", "Бразилия"],
+        ["Багамы", "Нассау"],
+        ["Бутан", "Тхимпху"],
+        ["Ботсвана", "Габороне"],
+        ["Беларусь", "Минск"],
+        ["Белиз", "Бельмопан"],
+        ["Канада", "Оттава"],
+        ["Демократическая Республика Конго", "Киншаса"],
+        ["Центральная Африканская Республика", "Банги"],
+        ["Швейцария", "Берн"],
+        ["Кот-д'Ивуар", "Ямусукро"],
+        ["Острова Кука", "Аваруа"],
+        ["Чили", "Сантьяго"],
+        ["Камерун", "Яунде"],
+        ["Китай", "Пекин"],
+        ["Колумбия", "Богота"],
+        ["Коста-Рика", "Сан-Хосе"],
+        ["Куба", "Гавана"],
+        ["Острова Зеленого Мыса", "Прая"],
+        ["Кипр", "Никосия"],
+        ["Чешская Республика", "Прага"],
+        ["Германия", "Берлин"],
+        ["Джибути", "Джибути"],
+        ["Дания", "Копенгаген"],
+        ["Доминиканская Республика", "Санто-Доминго"],
+        ["Алжир", "Алжир"],
+        ["Эквадор", "Кито"],
+        ["Эстония", "Таллин"],
+        ["Египет", "Каир"],
+        ["Западная Сахара", "Эль-Аюн"],
+        ["Эритрея", "Асмэра"],
+        ["Испания", "Мадрид"],
+        ["Эфиопия", "Аддис-Абеба"],
+        ["Финляндия", "Хельсинки"],
+        ["Фиджи", "Сува"],
+        ["Микронезия", "Паликир"],
+        ["Фарерские о-ва", "Торсхавн"],
+        ["Франция", "Париж"],
+        ["Габон", "Либревиль"],
+        ["Великобритания", "Лондон"],
+        ["Гренада", "Сент-Джоржес"],
+        ["Грузия", "Тбилиси"],
+        ["Французская Гвиана", "Кайенна"],
+        ["Гернси", "Сент-Питер-Порт"],
+        ["Гана", "Аккра"],
+        ["Гибралтар", "Гибралтар"],
+        ["Гренландия", "Нуук"],
+        ["Гамбия", "Банжул"],
+        ["Гвинея", "Конакри"],
+        ["Гваделупа", "Бас-Тер"],
+        ["Экваториальная Гвинея", "Малабо"],
+        ["Греция", "Афины"],
+        ["Гватемала", "Гватемала"],
+        ["Гуам", "Аганья"],
+        ["Гвинея-Биссау", "Бисау"],
+        ["Гайана", "Джорджтаун"],
+        ["Гонконг", "Гонконг"],
+        ["Гондурас", "Тегусигальпа"],
+        ["Хорватия", "Загреб"],
+        ["Гаити", "Порт-о-Пренс"],
+        ["Венгрия", "Будапешт"],
+        ["Индонезия", "Джакарта"],
+        ["Ирландия", "Дублин"],
+        ["Мэн, о-в", "Дуглас"],
+        ["Индия", "Нью-Дели"],
+        ["Ирак", "Багдад"],
+        ["Иран", "Тегеран"],
+        ["Исландия", "Рейкьявик"],
+        ["Италия", "Рим"],
+        ["Джерси", "Сент-Хельер"],
+        ["Ямайка", "Кингстон"],
+        ["Иордания", "Амман"],
+        ["Япония", "Токио"],
+        ["Кения", "Найроби"],
+        ["Кыргызстан", "Бишкек"],
+        ["Камбоджа", "Пномпень"],
+        ["Кирибати", "Южная Тарава"],
+        ["Коморские о-ва", "Морони"],
+        ["Сент-Киттс и Невис", "Бастер"],
+        ["Северная Корея", "Пхеньян"],
+        ["Южная Корея", "Сеул"],
+        ["Кувейт", "Кувейт"],
+        ["Каймановы Острова", "Джоржтаун"],
+        ["Казахстан", "Астана"],
+        ["Лаос", "Вьентьян"],
+        ["Ливан", "Бейрут"],
+        ["Сент-Люсия", "Кастри"],
+        ["Лихтенштейн", "Вадуц"],
+        ["Шри-Ланка", "Коломбо"],
+        ["Либерия", "Монровия"],
+        ["Лесото", "Масеру"],
+        ["Литва", "ВИЛЬНЮС"],
+        ["Люксембург", "Люксембург"],
+        ["Латвия", "Рига"],
+        ["Ливия", "Триполи"],
+        ["Марокко", "Рабат"],
+        ["Монако", "Монако"],
+        ["Молдова", "Кишинев"],
+        ["Черногория", "Подгорица"],
+        ["Остров Святого Мартина", "Мариго"],
+        ["Мадагаскар", "Антананариву"],
+        ["Маршалловы о-ва", "Маджуро"],
+        ["Македония", "Скопье"],
+        ["Мали", "Бамако"],
+        ["Мьянма", "Нейпьидо"],
+        ["Монголия", "Улан-Батор"],
+        ["Макао", "Макао"],
+        ["Северные Марианские о-ва", "Сайпан"],
+        ["Мартиника", "Форт-де-Франс"],
+        ["Мавритания", "Нуакшот"],
+        ["Монсеррат", "Плимут"],
+        ["Мальта", "Валлетта"],
+        ["Маврикий", "Порт-Луи"],
+        ["Мальдивские о-ва", "Мале"],
+        ["Малави", "Лилонгве"],
+        ["Мексика", "Мехико"],
+        ["Малайзия", "Куала-Лумпур"],
+        ["Мозамбик", "Мапуту"],
+        ["Намибия", "Виндхук"],
+        ["Новая Каледония", "Нумеа"],
+        ["Нигерия", "Ниамей"],
+        ["Остров Норфолк", "Кингстон"],
+        ["Нигерия", "Абуджа"],
+        ["Никарагуа", "Манагуа"],
+        ["Нидерланды", "Амстердам"],
+        ["Норвегия", "Осло"],
+        ["Непал", "Катманду"],
+        ["Новая Зеландия", "Веллингтон"],
+        ["Оман", "Маскат"],
+        ["Панама", "Панама"],
+        ["Перу", "Лима"],
+        ["Французская Полинезия", "Папеэте"],
+        ["Папуа – Новая Гвинея", "Порт-Морсби"],
+        ["Филиппины", "Манила"],
+        ["Пакистан", "Исламабад"],
+        ["Польша", "Варшава"],
+        ["Пуэрто-Рико", "Сан-Хуан"],
+        ["Португалия", "Лиссабон"],
+        ["Парагвай", "Асунсьон"],
+        ["Катар", "Доха"],
+        ["Румыния", "Бухарест"],
+        ["Сербия", "Белград"],
+        ["Россия", "Москва"],
+        ["Руанда", "Кигали"],
+        ["Саудовская Аравия", "Эр-Рияд"],
+        ["Соломонские острова", "Хониара"],
+        ["Сейшельские о-ва", "Виктория"],
+        ["Судан", "Хартум"],
+        ["Швеция", "Стокгольм"],
+        ["Сингапур", "Сингапур"],
+        ["Остров Святой Елены", "Джеймстаун"],
+        ["Словения", "Любляна"],
+        ["Словакия", "Братислава"],
+        ["Сьерра-Леоне", "Фритаун"],
+        ["Сан-Марино", "Сан-Марино"],
+        ["Сенегал", "Дакар"],
+        ["Сомали", "Могадишу"],
+        ["Суринам", "Парамарибо"],
+        ["Южный Судан", "Джуба"],
+        ["Сан-Томе и Принсипе", "Сан-Томе"],
+        ["Сальвадор", "Сан-Сальвадор"],
+        ["Сирийская Арабская Респ.", "Дамаск"],
+        ["Свазиленд", "Мбабане"],
+        ["Чад", "Нджамена"],
+        ["Того", "Ломе"],
+        ["Таиланд", "Бангкок"],
+        ["Таджикистан", "Душанбе"],
+        ["Восточный Тимор", "Дили"],
+        ["Туркменистан", "Ашхабад"],
+        ["Тунис", "Тунис"],
+        ["Тонга", "Нукуалофа"],
+        ["Турция", "Анкара"],
+        ["Тринидад и Тобаго", "Порт-оф-Спейн"],
+        ["Тувалу", "Funafuti"],
+        ["Тайвань", "Тайбэй"],
+        ["Танзания", "Додома"],
+        ["Украина", "Киев"],
+        ["Уганда", "Кампала"],
+        ["США", "Вашингтон"],
+        ["Уругвай", "Монтевидео"],
+        ["Узбекистан", "Ташкент"],
+        ["Ватикан", "Ватикан"],
+        ["Сент-Винсент и Гренадины", "Кингстаун"],
+        ["Венесуэла", "Каракас"],
+        ["Британские Виргинские о-ва", "Роадтаун"],
+        ["Вьетнам", "Ханой"],
+        ["Вануату", "Порт-Вила"],
+        ["Уоллис и Футуна", "Мата-Уту"],
+        ["Самоа", "Апиа"],
+        ["Йемен", "Сана"],
+        ["Майотта", "Мамудзу"],
+        ["ЮАР", "Претория"],
+        ["Замбия", "Лусака"],
+        ["Зимбабве", "Хараре"]
+      ]
     };
   },
 
   created() {
-    if (sessionStorage.getItem("activeCard") != null)
-      this.activeCard = sessionStorage.getItem("activeCard");
-
     this.activeUrl = this.ajaxApi[this.activeCard][1];
-    for (let i = 0; i < this.ajaxApi[this.activeCard].length - 2; i++) {
-      this.activeQuestCard[i] = this.ajaxApi[this.activeCard][
-        i + 2
-      ].toLowerCase();
+    for (let i = 0; i < 3; i++) {
+      this.randomCountryIndex[i] = Math.round(Math.random() * 222);
+      this.activeQuestCard[i] = this.ajaxApi[
+        this.randomCountryIndex[i]
+      ][1].toLowerCase();
+      this.randomCountry[i] = this.ajaxApi[this.randomCountryIndex[i]][0];
       this.hintStatus[i] = false;
       this.indexInputLetters[i] = 0;
       this.inputStatus[i] = "";
@@ -135,12 +322,6 @@ export default {
       this.timerStopInput[i] = 0;
       this.timerFocusLost[i] = 0;
     }
-  },
-
-  computed: {
-    imageUrl() {
-      return require(`@/assets/img/${this.activeUrl}`);
-    },
   },
 
   mounted: function() {
@@ -216,6 +397,7 @@ export default {
       this.inputData[index] =
         this.inputData[index] +
         this.activeQuestCard[index][this.indexInputLetters[index]];
+      this.totalLetters++;
       this.$forceUpdate();
       this.correctInputLetters[index] = this.inputData[index];
       this.indexInputLetters[index] = this.indexInputLetters[index] + 1;
@@ -223,7 +405,7 @@ export default {
     },
 
     setFocus: function(item) {
-      const id = this.inputStatus.findIndex((elem) => elem != "is-valid");
+      const id = this.inputStatus.findIndex(elem => elem != "is-valid");
       if (id != -1 && this.inputStatus[item] != "is-valid" && item != null) {
         const input = document.getElementById(item);
         input.focus();
@@ -242,16 +424,36 @@ export default {
     getNextCard: function() {
       this.activeCard = parseInt(this.activeCard) + 1;
       if (this.activeCard == this.ajaxApi.length) this.activeCard = 0;
-      sessionStorage.setItem("activeCard", this.activeCard);
+      this.letterCount = sessionStorage.setItem(
+        "totalLetters",
+        this.totalLetters
+      );
+      /*       if (sessionStorage.getItem("totalLetters") != null) {
+        sessionStorage.setItem(
+          "totalLetters",
+          this.letterCount + this.totalLetters
+        );
+      } else sessionStorage.setItem("totalLetters", this.totalLetters);
+      this.totalLetters; */
       this.nextStatusButton = true;
       location.reload();
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
 div {
   user-select: none;
+}
+#country {
+  font-size: 35px;
+}
+#empty {
+  color: white;
+  font-size: 75px;
+}
+#count {
+  margin: auto;
 }
 </style>
