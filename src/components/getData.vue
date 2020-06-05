eb09773aaaf890c8f1af827908993f29f18446f1
Leonid Nevezhin <LNevezhin@gmail.com>
2020-06-05 03:36:59 +0300

    some deletes

--- src/components/getData.vue
+++
@@ -1,9 +0,0 @@
-data: url = "../src/assets/questions.json"
-axios
-  .get("../src/assets/questions.json")
-  .then(response => {
-    console.log("response", response);
-  })
-  .catch(error => {
-    console.log("error", error);
-  });
